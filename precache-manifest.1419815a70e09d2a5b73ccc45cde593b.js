cef511c1c9de9ca08a197ac06408505987899524
mentab <25363363+mentab@users.noreply.github.com>
2019-06-21 13:22:45 +0200

    Updates

---
+++ precache-manifest.1419815a70e09d2a5b73ccc45cde593b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a56a1a04425afff999211332b9a175e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "101771b3f848e85efc52",
+    "url": "/pomodoro-clock/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "101771b3f848e85efc52",
+    "url": "/pomodoro-clock/static/js/2.b471c775.chunk.js"
+  },
+  {
+    "revision": "5940fa9101bedf4834c8",
+    "url": "/pomodoro-clock/static/js/main.ec439152.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  }
+]);
